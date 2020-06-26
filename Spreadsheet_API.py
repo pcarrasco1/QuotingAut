8e7990e18cc3ea1e0a1dec8db64cd7d4f2d08d9f
pcarrasco1 <67110218+pcarrasco1@users.noreply.github.com>
2020-06-26 17:00:48 -0300

    Add files via upload

--- Spreadsheet_API.py
+++ Spreadsheet_API.py
@@ -1,11 +1,24 @@
 import gspread
-
+import pandas as pd
+from gspread_dataframe import get_as_dataframe, set_with_dataframe
+import numpy as np
 gc = gspread.oauth()
+ 
+
 
 def readData():
     sh = gc.open("quotingProjectAutomation")
     print(sh.sheet1.get('A1'))
+    worksheet = sh.sheet1
+    df = get_as_dataframe(worksheet, header= 0)
+    print(df)
 
+
+    
+
+
+
+    
 readData()
 
 
